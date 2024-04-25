cac8e2b2a33b5facbf425a117804f9d6747ca641
Dung Hoang <dunghoang@vulcanlabs.co>
2024-04-25 09:14:40 +0700

    update v1

--- lib_amazon/build.gradle.kts
+++ lib_amazon/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.internal.impldep.org.eclipse.jgit.lib.InflaterCache.release
-
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
@@ -27,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 }
 
