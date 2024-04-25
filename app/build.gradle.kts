10f4311d257f96c170376d1da0aad11ba09fd4ee
Dung Hoang <dunghoang@vulcanlabs.co>
2024-04-25 09:42:25 +0700

    update v1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.vulcanlabs.library.amazon"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.vulcanlabs.library.amazon"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
